32f57283c6241eb16299cafb103268ab5901a310
Nicola Vivarelli <nicola.vivarelli@edu.unife.it>
2024-10-02 15:40:00 +0200

    v3

--- Labs/1/12.s
+++ Labs/1/12.s
@@ -6,20 +6,23 @@ v3: .space 10
 .text
 main:
     daddui r1, r0, 10 # counter per v1
-    daddui r2, r0, 0 # i = 0
-    daddui r4, r0, 0 # k = 0
+    daddui r2, r0, 0 # r2 = 0
+    daddui r4, r0, 0 # r4 = 0
 
 loop_1:
         lb r5, v1(r2) # r5 = v1[0] 
         daddui r6, r0, 0 # counter per v2
         daddui r3, r0, 10 # j = 10
-
 loop_2:
         lb r7, v2(r6)  #r7 = v2[0]
         beq r5, r7, found  #if(v1[i] == v2[j]) then found
         daddi r6, r6, 1  #v2[j+1]
         daddui r3, r3, -1  #j--
-        bnez r6, loop_2 #if(j != 0) then loop_2 [torna su finchè j != 0]
+        bnez r6, loop_2 #if(r6 != 0) then loop_2 [torna su finchè j != 0]
+        
+        daddui r2, r2, 1 # i++
+        daddui r1, r1, -1 # pointer di v1--
+        bnez r1, loop_1 #if(r1 != 0) then loop_1 [torna su finchè i != 0]
 
 found:
         sb r5, v3(r4) #v3[k] = v1[i]
